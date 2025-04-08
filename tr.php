6d9d9d0e70217ecb3b9cdd65b0db56c48f87b9e4
dt10812 <ducthinh100812@gmail.com>
2025-04-08 22:23:56 +0700

    Update tr.php

--- tr.php
+++ tr.php
@@ -1,10 +1,16 @@
 <?php
-// Log user details
-$logFile = 'tr.log';
-$data = date('Y-m-d H:i:s') . " | IP: " . $_SERVER['REMOTE_ADDR'] . " | User Agent: " . $_SERVER['HTTP_USER_AGENT'] . " | Referrer: " . $_SERVER['HTTP_REFERER'] . "\n";
-file_put_contents($logFile, $data, FILE_APPEND);
+// Set the log file
+$logFile = 'tracking.log';
 
-// Set header to serve an image
+// Gather tracking details
+$data = date('Y-m-d H:i:s') . " | IP: " . $_SERVER['REMOTE_ADDR'] . 
+        " | User Agent: " . $_SERVER['HTTP_USER_AGENT'] . 
+        " | Referrer: " . ($_SERVER['HTTP_REFERER'] ?? 'Direct Visit') . "\n";
+
+// Write data to the log file
+file_put_contents($logFile, $data, FILE_APPEND | LOCK_EX);
+
+// Serve a transparent pixel
 header("Content-Type: image/png");
-readfile("logo.png"); // Ensure pixel.png is a transparent 1x1 image
+readfile("pixel.png"); // Make sure `pixel.png` exists and is a 1x1 transparent image
 ?>
